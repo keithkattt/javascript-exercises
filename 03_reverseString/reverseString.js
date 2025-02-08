3e464c00bb2cf29b382546b00b5929dd8c0e4f31
Keithkattt <keith110606@gmail.com>
2025-02-08 21:02:17 +0800

    Succesfull in exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) { 
+    let strLength = str.length;
+    let lettersOfString = str.split('');
+    let popLetter = '';
+    let reversedLetters = '';
 
+    for (i = 0; i < strLength; i++){
+        popLetter = lettersOfString.pop();
+        reversedLetters += popLetter;
+    }
+    
+    return reversedLetters;
 };
 
 // Do not edit below this line
