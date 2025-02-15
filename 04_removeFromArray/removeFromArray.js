da3947f5d5f02b84663ccee1ac8f3315a6fc2425
keithkattt <keith110606@gmail.com>
2025-02-15 19:14:02 +0800

    Add array filter to remove the items that are in the removeItems parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems) {
+    const filteredArray = array.filter(number => !removeItems.includes(number));
+    
+    return filteredArray
 };
 
 // Do not edit below this line
