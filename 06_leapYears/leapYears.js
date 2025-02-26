76d8d19c2ab3050b44074ed585d1a2a2bc729557
keithkattt <keith110606@gmail.com>
2025-02-27 01:38:25 +0800

    completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let isLeapYear = false;
+    
+    if (inputYear % 4 === 0 && inputYear % 100 === 0 && inputYear % 400 === 0){
+        isLeapYear = true;
+    } else {
+        if (inputYear % 4 === 0 && inputYear % 100 !== 0){
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
+    }
+    
+    
 
+    return isLeapYear;
 };
 
 // Do not edit below this line
